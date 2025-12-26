344c351103cd9b17e3af793964ad2d2b62d8b2e7
Ayush Umrao <umraoayush@gmail.com>
2025-12-26 22:13:59 +0530

    learning array #1

---
+++ array1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[3] = {3,4,5};
+    for (int i = 0; i < 3; i++)
+    {
+        printf(" %d " , arr[i]);
+        // printf("\n");
+    }
+    
+   
+    return 0;
+}
