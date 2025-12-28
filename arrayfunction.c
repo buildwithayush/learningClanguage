826e32c4cd45d06f098ccc911231f2a12a6bd071
Ayush Umrao <umraoayush@gmail.com>
2025-12-28 11:25:40 +0530

    swap two numbers in array

--- arrayfunction.c
+++ arrayfunction.c
@@ -1,16 +1,33 @@
-#include<stdio.h>
-void changeNumber(int x[]){
-    x[0] = 10;
-    return;
+#include <stdio.h>
+
+// void changeNumber(int x[]){
+//     x[0] = 10;
+//     return;
+// }
+
+// --------------------------------------------------------------
+
+void swap(int arr[])
+{
+    int temp = arr[0];
+    arr[0] = arr[1];
+    arr[1] = temp;
 }
 
+int main()
+{
+    int arr[] = {1, 2};
 
-int main(){
+    printf("%d %d \n", arr[0], arr[1]);
+    swap(arr);
+    printf("%d %d \n", arr[0], arr[1]);
 
     // array is passed by reference
-    int arr[5] = {1,2,3,4,5};
-    printf("%d \n" , arr[0]);
-    changeNumber(arr);
-    printf("%d  " , arr[0]);
-    
+
+    // int arr[5] = {1,2,3,4,5};
+    // printf("%d \n" , arr[0]);
+    // changeNumber(arr);
+    // printf("%d  " , arr[0]);
+
+    // ------------------------------------------------------------------
 }
