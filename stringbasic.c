1b39c2d4e74a1bd0c0ca456c56bcb2d8279662d4
Ayush Umrao <umraoayush@gmail.com>
2025-12-29 23:59:11 +0530

    Learn gets and puts In String

--- stringbasic.c
+++ stringbasic.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
-    char name[] = "Hello world";
+    char name[40];
    // int i = 0;
     // while(name[i]!= '\0') {
     //     printf("%c", name[i]);
@@ -9,6 +10,10 @@ int main() {
    // }
 
    // format specifier for string
-    printf("%s", name);
+   // printf("%s", name);
+    gets(name);
+    puts(name);
+    printf(" Output is %s", name);
+   // puts("Ayush Is Learning C In Midnight In Very Cold Weather");
     return 0;
 }
